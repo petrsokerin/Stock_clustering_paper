e9188cd8d5d5e3705394f3682b7368f39c7e76cd
PS_sc <petya4343@mail.ru>
2023-06-26 22:04:41 +0300

    remove autoencoders from embedding

--- embeddings/autoencoders/autoencoders/__init__.py
+++
@@ -1,4 +0,0 @@
-from .networks.cae import Conv1dAutoEncoder
-from .networks.lstm import LSTMAutoEncoder
-from .networks.mlp import MLPAutoEncoder
-from .datamodules.tickerdatamodule import TickerDataModule
